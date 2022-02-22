b2af9f9736c280b1e18f85364332a84f4f3561b5
mad-proton <52608404+mad-proton@users.noreply.github.com>
2022-02-22 12:27:01 +0600

    problem and contest filtration added

--- cf_tracker/contests.py
+++ cf_tracker/contests.py
@@ -1,18 +1,48 @@
-
-from flask import Blueprint, render_template, request
+from multiprocessing import Condition
+from tkinter.tix import Select
+from flask import Blueprint, redirect, render_template, request, url_for
 
 from cf_tracker.db import get_db, query_db
 
 bp = Blueprint("contests", __name__, url_prefix="/contests")
 
-@bp.route("/")
-def contests():
-    all_contests = query_db('''
-        SELECT CONTEST_ID, CONTEST_NAME, START_TIME, ('https://codeforces.com/contest/' || CONTEST_ID) CONTEST_LINK
-        FROM CONTESTS
-        ORDER BY START_TIME DESC
-    ''')
+@bp.route("/show_contests", methods=('GET', 'POST'))
+def show_contests():
+    if request.method == "POST":
+        # print("in post method")
+        contest_id = request.form["contest_id"]
+        print(f"contest_id = {contest_id}") 
+        return redirect(url_for("contests.show_contests", contest_id=contest_id))
+        
+    contest_id = request.args.get("contest_id")
+
+    if contest_id is None:
+        all_contests = query_db('''
+            SELECT CONTEST_ID, CONTEST_NAME, START_TIME, ('https://codeforces.com/contest/' || CONTEST_ID) CONTEST_LINK
+            FROM CONTESTS
+            ORDER BY START_TIME DESC
+        ''')
+    
+    else:
+        all_contests = query_db('''
+            SELECT CONTEST_ID, CONTEST_NAME, START_TIME, ('https://codeforces.com/contest/' || CONTEST_ID) CONTEST_LINK
+            FROM CONTESTS
+            WHERE contest_id = :contest_id
+            ORDER BY START_TIME DESC
+        ''', [contest_id])
 
     return render_template("contests/contests.html", all_contests=all_contests[:100])
+
+@bp.route("/contest/<int:contest_id>")
+def contest(contest_id):
+    contest_problems = query_db('''
+                      SELECT PROBLEM_INDEX, PROBLEM_NAME, PROBLEM_RATING
+                      FROM PROBLEMS
+                      WHERE CONTEST_ID=:contest_id 
+                      ORDER BY PROBLEM_INDEX ASC
+    ''', [contest_id])
+    print(contest_problems)
+    # return f"contest_id = {contest_id}"
+    return render_template("contests/contest_problems.html", contest_problems=contest_problems)
 
 
--- cf_tracker/problems.py
+++ cf_tracker/problems.py
@@ -1,21 +1,42 @@
-
+from flask import redirect, url_for
 from flask import Blueprint, render_template, request
 
 from cf_tracker.db import get_db, query_db
 
 bp = Blueprint("problems", __name__, url_prefix="/problems")
 
-@bp.route("/")
-@bp.route("/<problem_id>")
+@bp.route("/problems", methods=('GET', 'POST'))
 def problems(problem_id=None):
+
+    if request.method == "POST":
+        # print("in post method")
+        problem_rating_low = request.form["problem_rating_low"]
+        problem_rating_high = request.form["problem_rating_high"]
+        print(f"problem_rating_low = {problem_rating_low}") 
+        print(f"problem_rating_high = {problem_rating_high}")
+        if problem_rating_high == "":
+            problem_rating_high=4000
+        if problem_rating_low == "":
+            problem_rating_low=800
+        return redirect(url_for("problems.problems", problem_rating_low=problem_rating_low, problem_rating_high=problem_rating_high))
+
+    rating_low = request.args.get("problem_rating_low", 800)
+    rating_high = request.args.get("problem_rating_high", 4000)
+
+    print(rating_high)
+    print(rating_low)
     all_problems = query_db('''
         SELECT CONTEST_ID, PROBLEM_INDEX, PROBLEM_NAME, PROBLEM_RATING,
             (TO_CHAR(CONTEST_ID) || PROBLEM_INDEX) PROBLEM_ID, 
             ('https://codeforces.com/contest/' || TO_CHAR(CONTEST_ID) || '/problem/' || PROBLEM_INDEX) PROBLEM_LINK
         FROM PROBLEMS
+        WHERE PROBLEM_RATING >= :rating_low AND PROBLEM_RATING <= :rating_high
         ORDER BY PROBLEM_RATING
-        ''')
+        ''', [rating_low, rating_high])
 
-    return render_template("problems/problems.html", all_problems=all_problems[:100])
+    return render_template("problems/problems.html", all_problems=all_problems)
+
+
+
 
 
